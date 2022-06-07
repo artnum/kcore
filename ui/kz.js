82341851524857694e029e42640352882690bf58
artnum <etienne@artisan-numerique.ch>
2022-06-07 09:34:42 +0200

    set zindex origin

--- ui/kz.js
+++ ui/kz.js
@@ -2,3 +2,11 @@ function KZ() {
     if (!KZ._current) { KZ._current = 0}
     return ++KZ._current
 }
+
+KZ.init = function (value) {
+    KZ._current = value
+}
+
+KZ.isInit = function () {
+    return KZ._current
+}
