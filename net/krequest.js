8dc06872038f966b1e773a4a345ef928f3b49ff9
artnum <etienne@artisan-numerique.ch>
2022-07-15 11:51:15 +0200

    fix scope issue

--- net/krequest.js
+++ net/krequest.js
@@ -1,6 +1,7 @@
+var KREQUEST = {}
 function krequest (url, options = {}) {
     return new Promise((resolve, reject) => {
-        if (!krequest._queries) {
+        if (!KREQUEST._queries) {
             const krequest_blob = new Blob([`self.onmessage = function (message) {
                 const request = message.data
                 const xreqid = request.options.headers['X-Request-Id']
@@ -43,15 +44,15 @@ function krequest (url, options = {}) {
                     self.postMessage({xreqid, error: {name: error.name, message: error.message, headers: error.headers ? error.headers : [], isHTTP: error.http ? true : false}})
                 })
             }`], {type: 'application/javascript'})
-            krequest._id = 0
-            krequest._queries = new Map()
-            krequest._worker = new Worker(URL.createObjectURL(krequest_blob))
-            krequest._worker.onmessage = function (message) {
+            KREQUEST._id = 0
+            KREQUEST._queries = new Map()
+            KREQUEST._worker = new Worker(URL.createObjectURL(krequest_blob))
+            KREQUEST._worker.onmessage = function (message) {
                 const response = message.data
 
                 if (!response.xreqid) { return }
-                if (!krequest._queries.has(xreqid)) { return }
-                const [resolve, reject] = krequest._queries.get(xreqid)
+                if (!KREQUEST._queries.has(xreqid)) { return }
+                const [resolve, reject] = KREQUEST._queries.get(xreqid)
                 if (response.error) { return reject(response.error) }
                 /* response.content contains two fields : body and headers */
                 return resolve(response.content)
@@ -73,14 +74,14 @@ function krequest (url, options = {}) {
             }
         }
         if (options.headers['X-Request-Id'] === undefined) {
-            xreqid = options.headers['X-Request-Id'] = ++krequest._id
+            xreqid = options.headers['X-Request-Id'] = ++KREQUEST._id
         } else {
             xreqid = options.headers['X-Request-Id']
         }
 
 
-        krequest._queries.set(xreqid, [resolve, reject])
-        krequest._worker.postMessage({
+        KREQUEST._queries.set(xreqid, [resolve, reject])
+        KREQUEST._worker.postMessage({
             url: url instanceof URL ? url.toString() : url,
             options: options
         })
